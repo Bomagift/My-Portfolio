daa1e8d8b8d515066c41db7662921f6a49573b61
boma <94929211+Bomagift@users.noreply.github.com>
2024-07-06 14:17:52 -0400

    corrected my images

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,12 +10,11 @@ function About() {
         <h1>Hi, I'm Boma Gift Obomanu</h1>
         <h2>Frontend Developer</h2>
         <p>
-          I am a passionate web developer with expertise
-          in React.js ,Css and Javascript. 
+           in React.js ,Css and Javascript. 
           I love creating visually appealing
           and user-friendly websites.  </p>
           </div>
-       <img src="public/images/boma picture.png" alt="" id="image" />
+       <img src="images/boma picture.png" alt="" id="image" />
 
       </section>
      
