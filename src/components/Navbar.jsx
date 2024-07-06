ab4b0294a888f75b4a4d94e0d52c97084bacef5d
boma <94929211+Bomagift@users.noreply.github.com>
2024-07-06 12:30:03 -0400

    Did my navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,42 @@
-import  { useState } from "react";
+import { useState } from "react";
 import "../Styles/Navbar.css";
+
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
+
   return (
     <nav className="navbar">
-      
       <div className="navbar-logo">
-        
         <a href="#home">Bo<span>ma</span></a>
       </div>
-      <ul className={`navbar-links ${isMenuOpen ? "open" : ""}`}>
-        
-        <li>
-          <a href="#home"> <span className="ho-me">Home</span></a>
-        </li>
-        <li>
-          <a href="#about">About</a>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-      </ul>
+      <div className={`navbar-menu ${isMenuOpen ? "open" : ""}`}>
+        <ul className="navbar-links">
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#projects">Projects</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
       <div className="navbar-toggle" onClick={toggleMenu}>
-        
-        {isMenuOpen ? "✖" : "☰"} {}
+        <div className={`fries-icon ${isMenuOpen ? "open" : ""}`}>
+          <div className="fries-bar"></div>
+          <div className="fries-bar"></div>
+          <div className="fries-bar"></div>
+        </div>
       </div>
     </nav>
   );
 };
+
 export default Navbar;
